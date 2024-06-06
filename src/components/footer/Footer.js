8f6936cb9e15abdba82596b13006e4c34086f802
Swapneel <vswapneel27@gmail.com>
2024-06-06 11:40:17 -0400

    2nd Commit: Added links and mail

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p>&copy; Adding my Linkedin here</p>
+        <p className={styles.name}>&copy; <a target="none" href="https://www.linkedin.com/in/swapneel-vaidya/">Swapneel Suhas Vaidya</a></p>
+        <p className={styles.mail}><a target="none" href="mailto:vswapneel27@gmail.com">vswapneel27@gmail.com</a></p>
       </div>
     </footer>
   );
